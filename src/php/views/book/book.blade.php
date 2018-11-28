1c932bd9beaf1e8b5099caf3742308ed0dae77e6
David Öhlin <hello@davidohl.in>
2018-11-28 11:45:05 +0100

    darker lines under h2s

--- src/php/views/book/book.blade.php
+++ src/php/views/book/book.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title></title>
-    <link rel="stylesheet" type="text/css" href="{{ get_stylesheet_directory_uri() }}/assets/dist/css/print.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ PRINT_TR_PLUGIN_URL }}/dist/css/print.min.css">
 </head>
 <body>
     {{-- Cover Page (Empty) --}}
@@ -19,19 +19,19 @@
                 <li class="table-of-contents__subchapter">{{$children->post_title}}<a href="#{{$key+1}}.{{$k+1}}"></a></li>
             @endforeach
         @endforeach
-    </ul>    
+    </ul>
     {{-- Table of Contents END --}}
-
+    
     <main class="main" role="main">
         {{-- Book --}}
-		@foreach($chapters as $key => $chapter)
+        @foreach($chapters as $key=>$chapter)
                 <div class="section" id="{{$key+1}}">
                     <h1>{{$chapter->post_title}}</h1>
                     {!! apply_filters('the_content', $chapter->post_content) !!}
                     <div class="chapter-header-left">1</div>
                     <div class="chapter-header-right">1</div>
-                    @foreach($chapter->children as $k => $children)
-                        <h2 id="{{$key+1}}.{{$k+1}}">{{ $children->post_title }}</h2>
+                    @foreach($chapter->children as $k=>$children)
+                        <h2 id="{{$key+1}}.{{$k+1}}">{{$children->post_title}}</h2>
                         {!! apply_filters('the_content', $children->post_content) !!}
                     @endforeach
                 </div>
