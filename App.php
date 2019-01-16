8175d17308481dc65a37ecd3a3eed529bcee37a5
Hydén Roland RGS VS SYSTEMUTV IT <rhn014@lthalland.se>
2019-01-16 11:46:16 +0100

    Tryck ssk

--- App.php
+++ App.php
@@ -44,7 +44,7 @@ class App
 	 */
 	public function createOptionsPage() 
 	{
-		$title = 'Skapa PDF (27)';
+		$title = 'Skapa PDF (28)';
 		$slug = 'print-terapirekommendationer';
 
 		// Add options page
