e825ad8e46aadfddf6ed59ed824f8ab55ea38422
Hydén Roland RGS VS SYSTEMUTV IT <rhn014@lthalland.se>
2019-01-17 16:46:06 +0100

    Tryck för läkare

--- App.php
+++ App.php
@@ -45,7 +45,7 @@ class App
 	 */
 	public function createOptionsPage() 
 	{
-		$title = 'Skapa PDF (34)';
+		$title = 'Skapa PDF (35)';
 		$slug = 'print-terapirekommendationer';
 
 		// Add options page
