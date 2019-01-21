53b762b942f8cd3a633a96ed113a7967e33d32a6
Hydén Roland RGS VS SYSTEMUTV IT <rhn014@lthalland.se>
2019-01-21 13:29:56 +0100

    tryck för ssk

--- src/php/views/book/book-reklistorSsk.blade.php
+++ src/php/views/book/book-reklistorSsk.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title></title>
-    <link rel="stylesheet" type="text/css" href="{{ PRINT_TR_PLUGIN_URL }}/dist/css/print_ssk_a6.min.css?ver=7.0">
+    <link rel="stylesheet" type="text/css" href="{{ PRINT_TR_PLUGIN_URL }}/dist/css/print_ssk_a6.min.css?ver=8.0">
 </head>
 <body>
     
@@ -32,7 +32,7 @@
     <main class="main" role="main">
         {{-- Book --}}
         @foreach($chapters as $key => $chapter)
-            <div class="list" id="{{$key+1}}">
+            <div class="section" id="{{$key+1}}">
                 {!! apply_filters('the_content', $chapter['Content']) !!}
             </div>
         @endforeach
